6f88fc8e98666f5fb30d53e71a89bab3545fff23
igorjovita <igor.jovita@hotmail.com>
2023-10-25 12:12:16 -0300

    tes3

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ import streamlit as st
 import MySQLdb
 from dotenv import load_dotenv
 import pandas as pd
+import mysql.connector
 
 load_dotenv()
-mydb = MySQLdb.connect(
+mydb = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
     user=os.getenv("DB_USERNAME"),
     passwd=os.getenv("DB_PASSWORD"),
     db=os.getenv("DB_NAME"),
     autocommit=True,
-    ssl_mode="VERIFY_IDENTITY",
-    ssl={
-        "ca": "C:\ssl\certs\cacert.pem"
-    }
-)
+    ssl_verify_identity=True,
+    ssl_ca="C:\ssl\certs\cacert.pem")
 
 cursor = mydb.cursor()
 
