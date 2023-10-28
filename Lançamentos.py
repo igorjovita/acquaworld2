cbd17f158a2ef64f9af42305a5a8f11c39eda5e3
igorjovita <igor.jovita@hotmail.com>
2023-10-28 11:20:08 -0300

    Update Lançamentos.py

--- Lançamentos.py
+++ Lançamentos.py
@@ -200,7 +200,7 @@ if st.button('Lançar no Sistema'):
         texto_staff = f"{staffd1_formatado[1]} - {staffd1_formatado[0]}"
         st.code(texto_p1 + texto_staff + texto_p2)
 
-    if staffd1 != '' and staffd2 != '':
+    if staffd1 != '' and staffd2 != '' and instrutor == '':
         staffd1_formatado = staffd1.split(',')
         staffd2_formatado = staffd2.split(',')
         texto_staff2 = f"""
@@ -219,18 +219,21 @@ if st.button('Lançar no Sistema'):
     if staffd1 != '' and staffd2 != '' and instrutor != '' and instrutor2 != '':
         staffd1_formatado = staffd1.split(',')
         staffd2_formatado = staffd2.split(',')
-        texto_staff2 = f"""
+        texto_staff_curso2 = f"""
             {staffd1_formatado[1]} - {staffd1_formatado[0]}
-            {staffd2_formatado[1]} - {staffd2_formatado[0]}"""
-        st.code(texto_p1 + texto_staff2 + texto_curso + texto_curso2 + texto_p2)
+            {staffd2_formatado[1]} - {staffd2_formatado[0]}
+            {instrutor} - {quantidade} {curso} {pratica}
+            {instrutor2} - {quantidade2} {curso2} {pratica2}"""
+        st.code(texto_p1 + texto_staff_curso2 + texto_p2)
 
     if staffd1 != '' and staffd2 != '' and instrutor != '' and instrutor2 == '':
         staffd1_formatado = staffd1.split(',')
         staffd2_formatado = staffd2.split(',')
-        texto_staff2 = f"""
+        texto_staff_curso = f"""
             {staffd1_formatado[1]} - {staffd1_formatado[0]}
-            {staffd2_formatado[1]} - {staffd2_formatado[0]}"""
-        st.code(texto_p1 + texto_staff2 + texto_curso + texto_p2)
+            {staffd2_formatado[1]} - {staffd2_formatado[0]}
+            {instrutor} - {quantidade} {curso} {pratica}"""
+        st.code(texto_p1 + texto_staff_curso + texto_p2)
 
 # cursor.execute("SELECT * FROM lancamento_bat")
 # df = pd.DataFrame(cursor.fetchall(), columns=['ID', 'Data', 'Id_staff', 'Divisao', 'Situação'])
