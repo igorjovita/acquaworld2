157d5284eb1524c694b33f71131eb61605c16f41
igorjovita <igor.jovita@hotmail.com>
2023-10-28 11:49:47 -0300

    da

--- Lançamentos.py
+++ Lançamentos.py
@@ -200,8 +200,11 @@ if st.button('Lançar no Sistema'):
 
     if staffd1 != '' and staffd2 == '':
         staffd1_formatado = staffd1.split(',')
-        texto_staff = f"{staffd1_formatado[1]} - {staffd1_formatado[0]}"
-        st.code(texto_p1 + texto_staff + texto_p2)
+        texto_staff = f"""{staffd1_formatado[1]} - {staffd1_formatado[0]}
+            {divisao} - {lista_final}
+            {apoio_superficie} - {equipagens} equipagens
+            {mestre} - {embarques} embarques"""
+        st.code(texto_p1 + texto_staff)
 
     if staffd1 != '' and staffd2 != '' and instrutor == '':
         staffd1_formatado = staffd1.split(',')
