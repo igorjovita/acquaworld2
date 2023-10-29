b52e624e963b558e38b480066700464b68658123
igorjovita <igor.jovita@hotmail.com>
2023-10-28 22:54:53 -0300

    Update Cilindros.py

--- pages/Cilindros.py
+++ pages/Cilindros.py
@@ -17,11 +17,12 @@ cursor = mydb.cursor()
 now = datetime.now()
 data = st.date_input
 staff = st.selectbox('Staff', ['', 'Juarez', 'Glauber', 'Roberta'])
-hora_inicio = str(st.text_input('Horario de Inicio'))
-hora_final = str(st.text_input('Horario do Termino'))
 segundos = ':00'
-inicio = str(hora_inicio + segundos)
-final = str(hora_final + segundos)
+hora_inicio = str(st.text_input('Horario de Inicio') + segundos)
+hora_final = str(st.text_input('Horario do Termino') + segundos)
+
+inicio = f'{hora_inicio}'
+final = f'{hora_final}'
 quantidade_acqua = st.text_input('Cilindros Acqua')
 quantidade_pl = st.text_input('Cilindros PL')
 quentinha = st.selectbox('Almoço', ['', 'Sim', 'Não'])
