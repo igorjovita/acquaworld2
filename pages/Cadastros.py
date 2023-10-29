5cad6d40f4d76f1c11075bdd23181c4c41dad4d2
igorjovita <igor.jovita@hotmail.com>
2023-10-29 09:00:37 -0300

    cadastro_colelte_regulador

--- pages/Cadastros.py
+++ pages/Cadastros.py
@@ -37,17 +37,47 @@ st.write('---')
 
 st.subheader('Cadastro Cilindro')
 
-marca = st.text_input('Marca do Cilindro')
+marca_cilindro = st.selectbox('Marca do Cilindro', ['', 'Luxfer', 'Catalina'])
 
-modelo = st.selectbox('Modelo do Cilindro', ['', 'Luxfer', 'Catalina'])
+modelo_cilindro = st.text_input('Modelo do Cilindro')
 
-serie = st.text_input('Numero de Serie do Cilindro')
+num_serie_cilindro = st.text_input('Numero de Serie do Cilindro')
 
-teste = st.date_input('Data do ultimo teste hidrostático')
+data_teste = st.date_input('Data do ultimo teste hidrostático')
 
-situacao = st.selectbox('Situação do Cilindro', ['', 'Em uso', 'Parado'])
+situacao_cilindro = st.selectbox('Situação do Cilindro', ['', 'Em uso', 'Parado'])
 
 if st.button('Cadastrar Cilindro'):
-    cursor.execute('INSERT INTO cadastro_cilindro( marca, modelo, num_serie, data_teste, situacao) VALUES (%s, %s, %s, %s, %s)',(marca, modelo, serie, teste, situacao))
+    cursor.execute('INSERT INTO cadastro_cilindro( marca, modelo, num_serie, data_teste, situacao) VALUES (%s, %s, %s, %s, %s)',(marca_cilindro, modelo_cilindro, num_serie_cilindro, data_teste, situacao_cilindro))
     st.success('Cilindro Cadastrado com sucesso!')
-    
+
+st.write('---')
+
+st.subheader('Cadastro Colete')
+
+marca_colete = st.text_input('Marca do Colete').capitalize()
+
+modelo_colete = st.text_input('Modelo do Colete').capitalize()
+
+tamanho_colete = st.text_input('Tamanho do Colete').capitalize()
+
+estado_colete = st.selectbox('Estado do Colete', ['', 'Operacional', 'Furado', 'Em manutenção'])
+
+if st.button('Cadastrar Colete'):
+    cursor.execute("INSERT INTO cadastro_colete ( marca, modelo, tamanho, estado) VALUES (%s, %s, %s, %s)",(marca_colete, modelo_colete, tamanho_colete, estado_colete))
+    st.success('Colete Cadastrado com Sucesso!')
+
+st.write('---')
+
+st.subheader('Cadastro Regulador')
+
+modelo_regulador = st.text_input('Modelo do Regulador')
+
+numercao_regulador = st.text_input('Numeração do Regulador')
+
+estado_regulador = st.selectbox('Estado do Regulador', ['', 'Operacional', 'Com vazamento', 'Em manutenção'])
+
+if st.button('Cadastrar Regulador'):
+    cursor.execute("INSERT INTO cadastro_regulador ( modelo, numeracao, estado) VALUES (%s, %s, %s)", (modelo_regulador, numercao_regulador, estado_regulador))
+    st.success('Regulador Cadastrado com Sucesso!')
+
