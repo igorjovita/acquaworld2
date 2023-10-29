e2c252cb605f9556fb18f88ed8c8037742e7cf4f
igorjovita <igor.jovita@hotmail.com>
2023-10-28 22:51:06 -0300

    Update Cilindros.py

--- pages/Cilindros.py
+++ pages/Cilindros.py
@@ -33,4 +33,5 @@ if st.button('Lançar no Sistema'):
     cursor.execute("INSERT INTO lancamento_cilindro (data, id_staff, horario_inicio, horario_final, cilindros_acqua, clindros_pl, almoço, situacao) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)",(data, staff, inicio, final, quantidade_acqua, quantidade_pl, quentinha, situacao))
     mydb.commit()
     st.success('Lançado no Sistema com Sucesso!')
-
+    st.write(inicio)
+    st.write(final)
