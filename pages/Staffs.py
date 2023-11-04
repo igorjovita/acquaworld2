a6d265f6469512b657fcd18441aea2fb4db9a4b3
igorjovita <igor.jovita@hotmail.com>
2023-11-04 11:54:42 -0300

    Update Staffs.py

--- pages/Staffs.py
+++ pages/Staffs.py
@@ -80,6 +80,5 @@ if st.button('Atualizar Status'):
     cursor.execute(f"Update staffs set status = '{status}' where nome = '{nome}'")
     mydb.commit()
     mydb.close()
-    time.sleep(0.5)
-    st.experimental_rerun()
+
 
