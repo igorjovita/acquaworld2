508cefeec1437b7b8cb9afc52cf449f7f959c5a7
igorjovita <igor.jovita@hotmail.com>
2023-11-04 18:44:11 -0300

    Update Financeiro.py

--- pages/Financeiro.py
+++ pages/Financeiro.py
@@ -20,7 +20,7 @@ st.title('Sistema AcquaWorld')
 st.header('Financeiro')
 
 st.subheader('Selecione o intervalo da pesquisa')
-data1 = st.date_input('Data Inicial', format='DD/MM/YYYY', value='yesterday')
+data1 = st.date_input('Data Inicial', format='DD/MM/YYYY', value=['2023-11-01'])
 data2 = st.date_input('Data Final', format='DD/MM/YYYY')
 
 cursor.execute(f"select data, id_staff, divisao from lancamento_bat where data between '{data1}' and '{data2}'")
