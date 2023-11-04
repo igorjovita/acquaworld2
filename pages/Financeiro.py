53a8bffb5b8765ac0b4d5b7fa9527bebe97e58ff
igorjovita <igor.jovita@hotmail.com>
2023-11-04 20:27:25 -0300

    Update Financeiro.py

--- pages/Financeiro.py
+++ pages/Financeiro.py
@@ -26,6 +26,6 @@ data2 = st.date_input('Data Final', format='DD/MM/YYYY')
 
 cursor.execute(f"select data, id_staff, divisao from lancamento_bat where data between '{data1}' and '{data2}'")
 lista = cursor.fetchall()
-st.write(lista)
+st.table(lista)
 
 
