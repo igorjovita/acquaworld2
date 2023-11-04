2544a3584546c64792abd8c19d98faa0e858b7f5
igorjovita <igor.jovita@hotmail.com>
2023-11-04 11:41:52 -0300

    Update Lançamentos.py

--- Lançamentos.py
+++ Lançamentos.py
@@ -31,11 +31,10 @@ st.write('''<style>
     flex: 1 1 calc(33.3333% - 1rem) !important;
     min-width: calc(33% - 1rem) !important;
 }
-.css-1l269bu {max-width:20% !important;}
 </style>''', unsafe_allow_html=True)
 
 for i, item in enumerate(lista_staffs):
-    done = st.write(str(item), key=str(i))
+    done = st.checkbox(str(item), key= str(i))
     # col1, col2, _ = st.columns([0.05, 0.8, 0.15])
     # done = col1.checkbox("a", key=str(i), label_visibility="hidden")
     #
