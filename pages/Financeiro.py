1342ba9ca9f064c36c4e5547e52f310fbc5d04a8
igorjovita <igor.jovita@hotmail.com>
2023-11-06 16:06:26 -0300

    Update Financeiro.py

--- pages/Financeiro.py
+++ pages/Financeiro.py
@@ -26,7 +26,7 @@ data2 = st.date_input('Data Final', format='DD/MM/YYYY')
 
 cursor.execute(f" select id_staff, sum(divisao) from lancamento_bat where data between '{data1}' and '{data2}' group by id_staff")
 lista = cursor.fetchall()
-df = pd.DataFrame(lista, columns=['Data', 'Staff', 'Divisão'])
+df = pd.DataFrame(lista, columns=['Staff', 'Divisão'])
 st.dataframe(df)
 
 
