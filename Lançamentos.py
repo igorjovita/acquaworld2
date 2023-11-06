5b2b264e603e05beb4229f970945a8b129a1400f
igorjovita <igor.jovita@hotmail.com>
2023-11-06 16:26:35 -0300

    Update Lançamentos.py

--- Lançamentos.py
+++ Lançamentos.py
@@ -15,7 +15,7 @@ cursor = mydb.cursor(buffered=True)
 
 chars = "'),([]"
 chars2 = "')([]"
-cursor.execute("SELECT nome FROM staffs where status = 'Ativo' and ocupacao = 'Instrutor' or 'Divemaster'")
+cursor.execute("SELECT nome FROM staffs where status = 'Ativo' and ocupacao = 'Instrutor' or ocupacao = 'Divemaster'")
 lista_staffs = str(cursor.fetchall()).translate(str.maketrans('', '', chars)).split()
 staffs_selecionados = []
 
