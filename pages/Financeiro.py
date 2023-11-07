dab269f13af1c0c79604085a99859091671594a6
igorjovita <igor.jovita@hotmail.com>
2023-11-07 15:33:23 -0300

    Update Financeiro.py

--- pages/Financeiro.py
+++ pages/Financeiro.py
@@ -24,31 +24,32 @@ st.subheader('Selecione o intervalo da pesquisa')
 data1 = st.date_input('Data Inicial', format='DD/MM/YYYY', value=datetime.date(year=2023, month=11, day=0o1))
 data2 = st.date_input('Data Final', format='DD/MM/YYYY')
 
-cursor.execute(f" select id_staff, sum(divisao) as divisao from lancamento_bat where data between '{data1}' and '{data2}' group by id_staff order by divisao desc")
-lista = cursor.fetchall()
+if st.button('Pesquisar'):
+    cursor.execute(f" select id_staff, sum(divisao) as divisao from lancamento_bat where data between '{data1}' and '{data2}' group by id_staff order by divisao desc")
+    lista = cursor.fetchall()
 
-for item in lista:
-    id_staff = item[0]
-    divisao = item[1]
-    mydb.connect()
-    col1, col2, col3 = st.columns(3)
-    with col1:
+    for item in lista:
+        id_staff = item[0]
+        divisao = item[1]
         mydb.connect()
-        cursor.execute(f"SELECT nome from staffs where id_staff = {id_staff}")
-        nome_staff = (str(cursor.fetchall()).translate(str.maketrans('', '', chars)))
-        mydb.close()
-        st.subheader(nome_staff)
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            mydb.connect()
+            cursor.execute(f"SELECT nome from staffs where id_staff = {id_staff}")
+            nome_staff = (str(cursor.fetchall()).translate(str.maketrans('', '', chars)))
+            mydb.close()
+            st.subheader(nome_staff)
 
-    with col2:
-        st.subheader(divisao)
+        with col2:
+            st.subheader(divisao)
 
-    with col3:
-        mydb.connect()
-        cursor.execute(f"SELECT comissao from staffs where id_staff = '{id_staff}'")
-        comissao_staff = (str(cursor.fetchall()).translate(str.maketrans('', '', chars)))
-        mydb.close()
-        valor_pagar = float(divisao) * int(comissao_staff)
-        st.subheader(f'R$ {valor_pagar}')
+        with col3:
+            mydb.connect()
+            cursor.execute(f"SELECT comissao from staffs where id_staff = '{id_staff}'")
+            comissao_staff = (str(cursor.fetchall()).translate(str.maketrans('', '', chars)))
+            mydb.close()
+            valor_pagar = float(divisao) * int(comissao_staff)
+            st.subheader(f'R$ {valor_pagar}')
 
 
 
