deae83c32a49cf79021ba9d946842081d594d856
igorjovita <igor.jovita@hotmail.com>
2023-11-07 15:56:35 -0300

    Update Financeiro.py

--- pages/Financeiro.py
+++ pages/Financeiro.py
@@ -23,11 +23,11 @@ st.header('Financeiro')
 st.subheader('Selecione o intervalo da pesquisa')
 data1 = st.date_input('Data Inicial', format='DD/MM/YYYY', value=datetime.date(year=2023, month=11, day=0o1))
 data2 = st.date_input('Data Final', format='DD/MM/YYYY')
-escolha = st.selectbox('Escolha o tipo', ['Comissão Staff', 'Comissao AS', 'Comissao Capitao', 'Comissao Cilindro'])
+escolha = st.selectbox('Escolha o tipo', ['Comissão Staff', 'Comissão AS', 'Comissão Capitao', 'Comissão Cilindro'])
 botao = st.button('Pesquisar')
 
 if botao:
-    if escolha == 'Comissao Staff':
+    if escolha == 'Comissão Staff':
         cursor.execute(f" select id_staff, sum(divisao) as divisao from lancamento_bat where data between '{data1}' and '{data2}' group by id_staff order by divisao desc")
         lista = cursor.fetchall()
 
@@ -52,7 +52,7 @@ if botao:
                 mydb.close()
                 valor_pagar = float(divisao) * int(comissao_staff)
                 st.subheader(f'R$ {valor_pagar}')
-    if escolha == 'Comissao AS':
+    if escolha == 'Comissão AS':
         cursor.execute(
             f" select id_staff, sum(equipagens) as equipagens from lancamento_as where data between '{data1}' and '{data2}' group by id_staff order by equipagens desc")
         lista = cursor.fetchall()
@@ -73,7 +73,7 @@ if botao:
                 st.subheader(equipagens)
 
 
-    if escolha == 'Comissao Capitao':
+    if escolha == 'Comissão Capitao':
         cursor.execute(f" select id_staff, sum(embarques) as embarques from lancamento_mestre where data between '{data1}' and '{data2}' group by id_staff order by embarques desc")
         lista = cursor.fetchall()
 
