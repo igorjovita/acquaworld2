84b50c593f5d7a84c3fd894126eb9a29afd09a47
igorjovita <igor.jovita@hotmail.com>
2023-11-08 13:50:16 -0300

    Update Financeiro.py

--- pages/Financeiro.py
+++ pages/Financeiro.py
@@ -196,6 +196,6 @@ if botao:
                 st.write('---')
                 mydb.connect()
                 cursor.execute(f"SELECT media_tempo FROM lancamento_cilindro where data between '{data1}' and '{data2}'")
-                tempo_medio = float((str(cursor.fetchone()).translate(str.maketrans('', '', chars))))
+                tempo_medio = (str(cursor.fetchone()).translate(str.maketrans('', '', chars)))
                 df = pd.DataFrame(tempo_medio, columns=['Medias'])
                 st.bar_chart(df)
