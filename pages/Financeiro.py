d3cae1c87f89591dc6bb79722c935cefb7d561b9
igorjovita <igor.jovita@hotmail.com>
2023-11-07 22:38:59 -0300

    Update Financeiro.py

--- pages/Financeiro.py
+++ pages/Financeiro.py
@@ -151,8 +151,8 @@ if botao:
                     hora_inicio = timedelta(hours=float(h1[0]), minutes=float(h1[1]))
                     hora_final = timedelta(hours=float(h2[0]), minutes=float(h2[1]))
                     horas_trabalhadas = hora_final - hora_inicio
-                    h3 = str(horas_trabalhadas)
-                    media_cilindro = float(h3)/(cilindros_acqua + cilindros_pl)
+                    h3 = horas_trabalhadas.total_seconds()/60
+                    media_cilindro = h3/(cilindros_acqua + cilindros_pl)
                     mydb.close()
                     st.subheader(f'Horario Inicial : {horario_inicial}')
                     st.subheader(f'Horario Final : {horario_final}')
