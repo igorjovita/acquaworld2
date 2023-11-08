3516d57d61bb9ed97fd56694a98ced4cb488c62b
igorjovita <igor.jovita@hotmail.com>
2023-11-08 12:54:05 -0300

    Update Financeiro.py

--- pages/Financeiro.py
+++ pages/Financeiro.py
@@ -133,11 +133,6 @@ if botao:
             horario_inicial = (str(cursor.fetchone()).translate(str.maketrans('', '', chars)))
             cursor.execute(f"SELECT horario_final from lancamento_cilindro where data between '{data1}' and '{data2}'")
             horario_final = (str(cursor.fetchone()).translate(str.maketrans('', '', chars)))
-            h1 = horario_inicial.split(':')
-            h2 = horario_final.split(':')
-            hora_inicio = timedelta(hours=float(h1[0]), minutes=float(h1[1]))
-            hora_final = timedelta(hours=float(h2[0]), minutes=float(h2[1]))
-            horas_trabalhadas = hora_final - hora_inicio
             cursor.execute(f"select sum(horas_trabalhadas) from lancamento_cilindro where data between '{data1}' and '{data2}'")
             minutos = float((str(cursor.fetchone()).translate(str.maketrans('', '', chars))))
             mydb.close()
