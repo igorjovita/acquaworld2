20f8d9e2e33aebb3265837515fd0c34e90931f41
igorjovita <igor.jovita@hotmail.com>
2023-11-13 11:15:40 -0300

    Update Lançamentos.py

--- Lançamentos.py
+++ Lançamentos.py
@@ -28,7 +28,7 @@ st.write('''<style>
 }
 </style>''', unsafe_allow_html=True)
 
-escolha = option_menu(menu_title=None, options=['Lançamentos', 'Editar', 'Deletar'],
+escolha = option_menu(menu_title=None, options=['Lançar', 'Editar', 'Deletar'],
                       icons=['book', 'pencil-square', 'trash'],
                       orientation='horizontal')
 if escolha == 'Lançamentos':
