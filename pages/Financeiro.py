8a51dcf388d2fa37d3d17207330f90dc2d22c2d2
IgorJovita <igor.jovita@hotmail.com>
2024-02-03 17:08:57 -0300

    Update Financeiro.py

--- pages/Financeiro.py
+++ pages/Financeiro.py
@@ -268,7 +268,6 @@ if st.button('Pesquisar2'):
         (id_staff, data1_pagamento, data2_pagamento))
     dados = cursor.fetchall()
     dados_str = ''
-    st.write('oi')
     # Itera sobre cada tupla em 'dados'
     total_equipagens = 0
     total_bat = 0
@@ -337,6 +336,24 @@ if st.button('Pesquisar2'):
     # Agora, dados_str conterá todos os textos com quebras de linha entre eles
     st.code(dados_str + texto_equipagem + texto_curso + texto_bat)
 
+    cursor.execute(f"""SELECT
+    lb.data,
+    lb.funcao,
+    lb.quantidade,
+    lb.curso,
+    lb.pratica,
+    lb.quentinha,
+    lc.cilindros_acqua,
+    lc.cilindros_pl,
+    lc.almoco,
+    s.nome
+    FROM lancamentos_barco lb
+    JOIN lancamento_cilindro lc ON lb.id_staff = lc.id_staff
+    JOIN staffs s ON lb.id_staff = s.id_staff
+    WHERE lb.data between {data1_pagamento} and {data2_pagamento} and id_staff = {id_staff};
+""")
+    dados2 = cursor.fetchall()
+    st.dataframe(dados2)
 
 
 
