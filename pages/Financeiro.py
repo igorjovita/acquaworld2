a2854d00deb687751a347077a8b57c2d82f390c9
IgorJovita <igor.jovita@hotmail.com>
2024-02-03 14:13:12 -0300

    Update Financeiro.py

--- pages/Financeiro.py
+++ pages/Financeiro.py
@@ -271,15 +271,21 @@ if st.button('Pesquisar2'):
     # Itera sobre cada tupla em 'dados'
     for dado in dados:
         st.write(f'Tupla atual: {dado}')
-        if len(dado) >= 5:  # Certifique-se de que há pelo menos 7 elementos na tupla
 
-            texto = f'{dado[0]} - {dado[1]} - {dado[2]} - {dado[3]} - {dado[4]} - {dado[5]} - {dado[6]}'
-        else:
-            texto = f'{dado[0]} - {dado[1]} - {dado[2]} - {dado[3]} - {dado[4]}'
+        # Certifica-se de que há pelo menos 5 elementos na tupla
+        if len(dado) >= 5:
+            # Construa o texto com base no número de elementos
+            if len(dado) == 6:
+                texto = f'{dado[0]} - {dado[1]} - {dado[2]} - {dado[3]} - {dado[4]} - {dado[5]}'
+            elif len(dado) == 5:
+                texto = f'{dado[0]} - {dado[1]} - {dado[2]} - {dado[3]} - {dado[4]}'
+            else:
+                texto = 'Número de elementos não esperado'
 
             # Adiciona o texto e uma quebra de linha ao final de dados_str
             dados_str += texto + '\n'
-
+        else:
+            st.warning(f'A tupla {dado} não possui o comprimento esperado.')
 
     # Agora, dados_str conterá todos os textos com quebras de linha entre eles
     st.code(dados_str)
