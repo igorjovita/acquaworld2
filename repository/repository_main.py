a4d608bc6840e3611c07f5352210261d2c80f4de
IgorJovita <igor.jovita@hotmail.com>
2024-04-26 13:31:23 -0300

    Update repository_main.py

--- repository/repository_main.py
+++ repository/repository_main.py
@@ -16,36 +16,38 @@ class MainRepository:
         SELECT 
             CONCAT(
                  staffs.nome, ' - ',
-                 SUM(CASE WHEN l.funcao = 'BAT' THEN l.quantidade ELSE 0 END), ' BAT', ' + ',
-                 SUM(CASE WHEN l.funcao = 'AS' THEN l.quantidade ELSE 0 END),' Equipagens' ' + ',
-                 SUM(CASE WHEN l.funcao = 'CAPITAO' THEN l.quantidade ELSE 0 END),' Embarques', ' + ',
-                 SUM(CASE WHEN l.funcao = 'CURSO' THEN l.quantidade ELSE 0 END), ' Curso' ' + ',
-                 COALESCE(SUM(lc.cilindros_acqua + lc.cilindros_pl), 0), ' Cilindros', ' + ',
-                 SUM(CASE WHEN l.quentinha = 'Sim' OR lc.almoco = 'Sim' THEN 1 ELSE 0 END),' Quentinhas', ' = ',
-                'R$ : ', SUM(
-                    CASE 
-                        WHEN l.id_staff IS NOT NULL THEN
-                            CASE 
-                                WHEN l.funcao = 'BAT' THEN l.quantidade * staffs.comissao
-                                WHEN l.funcao = 'AS' THEN l.quantidade * 1
-                                WHEN l.funcao = 'CAPITAO' THEN l.quantidade * 1
-                                WHEN l.funcao = 'CURSO' THEN 
-                                    CASE 
-                                        WHEN l.curso IN ('OWD', 'ADV') THEN l.quantidade * 75
-                                        WHEN l.curso = 'RESCUE' THEN l.quantidade * 150
-                                        WHEN l.curso = 'REVIEW' THEN l.quantidade * 120
-                                        WHEN l.curso = 'DIVEMASTER' THEN l.quantidade * 200
-                                        ELSE 0
-                                    END
-                                ELSE 0
-                            END +
-                            CASE 
-                                WHEN l.quentinha = 'Sim' THEN 15
-                                ELSE 0
-                            END
-                        ELSE
-                            0
-                    END
+                 CASE WHEN SUM(CASE WHEN l.funcao = 'BAT' THEN l.quantidade ELSE 0 END) > 0 THEN CONCAT(FORMAT(SUM(CASE WHEN l.funcao = 'BAT' THEN l.quantidade ELSE 0 END), 2), ' BAT + ') ELSE '' END,
+                 CASE WHEN SUM(CASE WHEN l.funcao = 'AS' THEN l.quantidade ELSE 0 END) > 0 THEN CONCAT(FORMAT(SUM(CASE WHEN l.funcao = 'AS' THEN l.quantidade ELSE 0 END), 2), ' Equipagens + ') ELSE '' END,
+                 CASE WHEN SUM(CASE WHEN l.funcao = 'CAPITAO' THEN l.quantidade ELSE 0 END) > 0 THEN CONCAT(FORMAT(SUM(CASE WHEN l.funcao = 'CAPITAO' THEN l.quantidade ELSE 0 END), 2), ' Embarques + ') ELSE '' END,
+                 CASE WHEN SUM(CASE WHEN l.funcao = 'CURSO' THEN l.quantidade ELSE 0 END) > 0 THEN CONCAT(FORMAT(SUM(CASE WHEN l.funcao = 'CURSO' THEN l.quantidade ELSE 0 END), 2), ' Curso + ') ELSE '' END,
+                 CASE WHEN COALESCE(SUM(lc.cilindros_acqua + lc.cilindros_pl), 0) > 0 THEN CONCAT(FORMAT(COALESCE(SUM(lc.cilindros_acqua + lc.cilindros_pl), 0), 2), ' Cilindros + ') ELSE '' END,
+                 CASE WHEN SUM(CASE WHEN l.quentinha = 'Sim' OR lc.almoco = 'Sim' THEN 1 ELSE 0 END) > 0 THEN CONCAT(FORMAT(SUM(CASE WHEN l.quentinha = 'Sim' OR lc.almoco = 'Sim' THEN 1 ELSE 0 END), 2), ' Quentinhas = ') ELSE '' END,
+                CONCAT('R$ : ', FORMAT(
+                    SUM(
+                        CASE 
+                            WHEN l.id_staff IS NOT NULL THEN
+                                CASE 
+                                    WHEN l.funcao = 'BAT' THEN l.quantidade * staffs.comissao
+                                    WHEN l.funcao = 'AS' THEN l.quantidade * 1
+                                    WHEN l.funcao = 'CAPITAO' THEN l.quantidade * 1
+                                    WHEN l.funcao = 'CURSO' THEN 
+                                        CASE 
+                                            WHEN l.curso IN ('OWD', 'ADV') THEN l.quantidade * 75
+                                            WHEN l.curso = 'RESCUE' THEN l.quantidade * 150
+                                            WHEN l.curso = 'REVIEW' THEN l.quantidade * 120
+                                            WHEN l.curso = 'DIVEMASTER' THEN l.quantidade * 200
+                                            ELSE 0
+                                        END
+                                    ELSE 0
+                                END +
+                                CASE 
+                                    WHEN l.quentinha = 'Sim' THEN 15
+                                    ELSE 0
+                                END
+                            ELSE
+                                0
+                        END
+                    ), 2)
                 )
             ) AS summary
         FROM 
@@ -54,9 +56,11 @@ class MainRepository:
             lancamentos_barco AS l ON staffs.id_staff = l.id_staff AND l.data BETWEEN %s AND %s
         LEFT JOIN 
             lancamento_cilindro AS lc ON staffs.id_staff = lc.id_staff AND l.data = lc.data
-        WHERE l.id_staff IS NOT NULL
+        WHERE 
+            l.id_staff IS NOT NULL
         GROUP BY 
             staffs.nome;
+
 
 
         """
