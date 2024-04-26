fd24f9c8cd346e91159d45011ad66a6708c86268
IgorJovita <igor.jovita@hotmail.com>
2024-04-26 10:47:37 -0300

    Update repository_main.py

--- repository/repository_main.py
+++ repository/repository_main.py
@@ -19,7 +19,7 @@ class MainRepository:
             SUM(CASE WHEN l.funcao = 'AS' THEN l.quantidade else 0 end) as total_equipagens,
             SUM(CASE WHEN l.funcao = 'CAPITAO' THEN l.quantidade else 0 end) as total_embarques,
             SUM(CASE WHEN l.funcao = 'CURSO' THEN l.quantidade else 0 end) as total_curso,
-            SUM(lc.cilindros_acqua + lc.cilindros_pl) as total_cilindro,
+            COALESCE(SUM(lc.cilindros_acqua + lc.cilindros_pl), 0) AS total_cilindros,
             CASE WHEN MAX(l.quentinha = 'Sim' OR lc.almoco = 'Sim') THEN 1 ELSE 0 END AS total_quentinhas,
             SUM(
                 CASE 
