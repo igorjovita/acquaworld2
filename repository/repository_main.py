92ba7ee600760a4df75f952bf994f553c838af1f
IgorJovita <igor.jovita@hotmail.com>
2024-04-30 17:00:28 -0300

    Update repository_main.py

--- repository/repository_main.py
+++ repository/repository_main.py
@@ -139,7 +139,7 @@ class MainRepository:
         """
 
 
-        params = (data_inicial, data_final, id_staff, data_inicial, data_final, id_staff)
+        params = (data_inicial, data_final, id_staff, data_inicial, data_final, id_staff, data_inicial, data_final, id_staff, id_staff)
 
         return self.db.execute_query(query, params)
 
