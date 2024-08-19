0989b1ed545b5e25b03eb1452ad0a8f06f9f7ccc
IgorJovita <igor.jovita@hotmail.com>
2024-08-19 10:09:26 -0300

    Update Estoque.py

--- pages/Estoque.py
+++ pages/Estoque.py
@@ -13,4 +13,16 @@ df = pd.DataFrame(select_cilindros, columns=['Marca', 'Numero de Serie', 'Teste'
 
 df['Teste'] = pd.to_datetime(df['Teste']).dt.strftime('%d/%m/%Y')
 
+cilindro_parado = 0
+cilindro_em_uso = 0
+for cilindro in select_cilindros:
+    if cilindro[3] == 'Em uso':
+        cilindro_em_uso += 1
+    elif cilindro[3] == 'Parado':
+        cilindro_parado += 1
+
+st.text(f'Cilindros em uso : {cilindro_em_uso}')
+st.text(f'Cilindros parados : {cilindro_parado}')
+
 st.table(df)
+
